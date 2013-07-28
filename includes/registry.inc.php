6ec57d16d10a1c982cfe508e094fd567e6f57fb0
|V1TR0 <xandi98@web.de>
2013-07-28 18:53:52 +0200

    less buggy registry

--- includes/registry.inc.php
+++ includes/registry.inc.php
@@ -1,29 +1,22 @@
 <?php
-
-    echo "<pre>";
-    print_r($_POST);    
-    echo "</pre>";
-    include_once 'classes/registry.class.php';
-
-    $registry = new registry();
-
-    if (isset($_POST["registerSubmit"])) {
-        $succeeded = $registry->register();
-        if($succeeded){
-            $tpl->addMainTemplate("registry.tpl.php");
-            $tpl->addCss(array("name" => "registry.css"));
-            $tpl->assign("Errors", $succeeded);
-            $tpl->assign("Title", "Registrierung");
-        }else{
-            $tpl->addMainTemplate("registryFinished.tpl.php");
-            $tpl->addCss(array("name" => "registryFinished.css"));
-            $tpl->assign("Title", "Abgeschlossene Registrierung");
-        }
-
+    if($_POST["registerSubmit"] == "1"){
+        echo "<pre>";
+        print_r($_POST);    
+        echo "</pre>"; 
+        
+        $tpl->addMainTemplate("registryFinished.tpl.php");
+        $tpl->addCss(array("name" => "registryFinished.css"));
+        $tpl->assign("Errors", $succeeded);
+        $tpl->assign("Title", "Abgeschlossene Registrierung");
+        
     } else {
-            $tpl->addMainTemplate("registry.tpl.php");
-            $tpl->addCss(array("name" => "registry.css"));
-            $tpl->assign("Title", "Registrierung");
-        }
-    
+        include_once 'classes/registry.class.php';
+
+        $registry = new registry();
+
+        $tpl->addMainTemplate("registry.tpl.php");
+        $tpl->addCss(array("name" => "registry.css"));
+        $tpl->assign("Errors", $succeeded);
+        $tpl->assign("Title", "Registrierung");
+    }
 ?>
