4d00a3cbbd0b27236b5f12b669249d71b8d92e54
|V1TR0 <xandi98@web.de>
2013-08-02 23:00:20 +0200

    added some stuff to overview class

--- includes/classes/overview.class.php
+++ includes/classes/overview.class.php
@@ -20,8 +20,7 @@ class overview {
     }
     
     public function getTmrwHomeworks(){
-        $sql = "SELECT 
-                    ID,
+        $sql = "SELECT
                     SubjectID,
                     Homework,
                     Subject
@@ -41,6 +40,50 @@ class overview {
         }else{
             return false;
         }
+    }
+    
+    public function getTmrwEvents(){
+            $sql = "SELECT
+                        Title,
+                        End
+                    FROM
+                        Events
+                    WHERE
+                        CURDATE() + 1
+                    BETWEEN
+                        Start
+                    AND
+                        End
+                    OR
+                        Start = CURDATE() + 1
+                    OR 
+                        End = CURDATE() + 1";
+            $result = $this->mysqli->query($sql);
+            return $result->fetch_object;
+    }
+    
+    public function getTmrwSchedule(){
+        $days = array("Monday" => "1", 
+                      "Tuesday" => "2", 
+                      "Wednesday" => "3",
+                      "Thursday" => "4",
+                      "Friday" => "5",
+                      "Saturday" => "6",
+                      "Sunday" => "7");
+        $sql = "SELECT
+                    Lesson,
+                    Subject
+                FROM
+                    Schedules
+                LEFT JOIN
+                    Subjects
+                ON
+                    Schedules.SubjectID = Subjects.ID
+                WHERE
+                    ClassID = '" . $user->getClassID() . "'
+                AND
+                    Day = '" . $days[date("l", $this->getTmrw())]. "';
+                ";
     }
 }
 
