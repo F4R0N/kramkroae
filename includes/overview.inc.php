dd6b09c3b762dd711d92d9a8aed83ca82cb8b4f8
|V1TR0 <xandi98@web.de>
2013-08-02 23:28:09 +0200

    fixed overview bug

--- includes/overview.inc.php
+++ includes/overview.inc.php
@@ -14,9 +14,6 @@ setlocale(LC_TIME, "de_DE");
 
 $tpl->assign("tomorrowDate", date("d. m. Y", $overview->getTmrw()));
 $tpl->assign("tomorrowDay", strftime("%A", $overview->getTmrw()));
-$tpl->assign("tmrwHWHomeworks", $overview->getTmrwHomeworks());
-$tpl->assign("tmrwHWEvents", $overview->getTmrwEvents());
-$tpl->assign("tmrwHWSchedule", $overview->getTmrwSchedule());
 $tpl->addMainTemplate("overview.tpl.php");
 $tpl->addCss(array("name" => "startPage.css"));
 
