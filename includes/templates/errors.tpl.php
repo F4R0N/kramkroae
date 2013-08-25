383eb2b4a465e02d17ff90b9bb9ac3c03a07045b
|V1TR0 <xandi98@web.de>
2013-08-25 22:17:36 +0200

    fixed german grammar bug

--- includes/templates/errors.tpl.php
+++ includes/templates/errors.tpl.php
@@ -1,5 +1,10 @@
 <h2>Fehler</h2>
-<?= count($VARS["Errors"]) ?> Fehler sind aufgetreten:
+<?= count($VARS["Errors"])?> Fehler
+<? if(count($VARS["Errors"]) == 1){ ?>
+    ist
+<? }else{
+    ?> sind <?
+} ?>aufgetreten:
 <ol>
 <?php foreach($VARS["Errors"] as $error): ?>
     <li><?= $error ?></li>
